1a1987fc71a2f3933d825c457d2bda6765f4db2c
Tezar Surya Fernanda <tzr.surya@gmail.com>
2022-03-10 16:32:42 +0700

    add priority to planet images

--- components/Planet.js
+++ components/Planet.js
@@ -3,7 +3,12 @@ import Image from "next/image";
 function Planet({ name }) {
   return (
     <div className="block relative min-w-[200px] min-h-[200px] sm:min-w-[300px] sm:min-h-[300px]">
-      <Image src={`/img/${name.toLowerCase()}.png`} layout="fill" alt={name} />
+      <Image
+        src={`/img/${name.toLowerCase()}.png`}
+        layout="fill"
+        alt={name}
+        priority
+      />
     </div>
   );
 }
