de3e74782bd9383f8f9858517900c0ca06b7b9b2
Tezar Surya Fernanda <tzr.surya@gmail.com>
2022-03-11 06:45:16 +0700

    configure workbox to cache .png

--- next.config.js
+++ next.config.js
@@ -3,6 +3,24 @@ const withOffline = require("next-offline");
 
 const nextConfig = {
   reactStrictMode: true,
+  workboxOpts: {
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "offlineCache",
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+      {
+        urlPattern: /.png$/,
+        handler: "CacheFirst",
+      },
+    ],
+  },
 };
 
 module.exports = withOffline(nextConfig);
